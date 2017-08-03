13c42122cf47b689f97171c0527835f8e9dfae96
ilsp <galanisd@gmail.com>
2017-08-03 22:26:56 +0300

    ** Updated.

--- omtd-component-dkpro-uima-1.9.0/src/main/java/eu/openminted/workflows/dkpro/executor/PipelineCommandLineRunner.java
+++ omtd-component-dkpro-uima-1.9.0/src/main/java/eu/openminted/workflows/dkpro/executor/PipelineCommandLineRunner.java
@@ -29,17 +29,22 @@ public class PipelineCommandLineRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		//String className = args[0]; 
-		//String inputDir = args[1];
-		//String outputDir = args[2];				
+		String className = args[0]; 
+		String inputDir = args[1];
+		String outputDir = args[2];				
 			
-		String className = "de.tudarmstadt.ukp.dkpro.core.io.pdf.PdfReader";
-		String inputDir = "C:/Users/galanisd/Desktop/Dimitris/EclipseWorkspaces/ILSPMars/omtd-workflows-executor/testInput/";
-		String outputDir = "C:/Users/galanisd/Desktop/Dimitris/EclipseWorkspaces/ILSPMars/omtd-workflows-executor/testOutput/";
+		//String className = "de.tudarmstadt.ukp.dkpro.core.io.pdf.PdfReader";
+		//String inputDir = "C:/Users/galanisd/Desktop/Dimitris/EclipseWorkspaces/ILSPMars/omtd-workflows-executor/testInput/";
+		//String outputDir = "C:/Users/galanisd/Desktop/Dimitris/EclipseWorkspaces/ILSPMars/omtd-workflows-executor/testOutput/";
 		
+		log.info("\n\n\n\n\nExecuting..");
+		log.info("========================");
 		log.info("className:" + className);
 		log.info("inputDir:" + inputDir);
 		log.info("outputDir:" + outputDir);
+		log.info("========================");
+		log.info("========================");
+		log.info("\n\n\n\n\n");
 		
 		UIMAFitRunner runner = new UIMAFitRunner();
 		runner.uimaFitRun(className, inputDir, outputDir);
