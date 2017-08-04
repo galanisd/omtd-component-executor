4f6a9a49fcc5310e9062398a7a3b78607578ae0f
ilsp <galanisd@gmail.com>
2017-08-05 01:03:38 +0300

    ** Updated .gitignore

---
+++ scripts/Linux_runDKProBerkeleyParser.sh
@@ -0,0 +1,6 @@
+coordinates=de.tudarmstadt.ukp.dkpro.core_de.tudarmstadt.ukp.dkpro.core.berkeleyparser-gpl_1.8.0
+className=de.tudarmstadt.ukp.dkpro.core.berkeleyparser.BerkeleyParser
+inD=../testOutput_de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter/
+otD=../testOutput_$className/
+
+bash ./Linux_runDKPro.sh $coordinates $className $inD $otD
