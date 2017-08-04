45fc8889fd9c83b8d5fad31f410b083727703744
ilsp <galanisd@gmail.com>
2017-08-05 01:00:36 +0300

    ** Renamed examples scripts ** Added Berkeley parser

--- scripts/Linux_runDKProArktweetTokenizer.sh
+++ scripts/Linux_runDKProArktweetTokenizer.sh
@@ -1,8 +1,6 @@
-#coordinates=de.tudarmstadt.ukp.dkpro.core_de.tudarmstadt.ukp.dkpro.core.opennlp-asl_1.8.0
 coordinates=de.tudarmstadt.ukp.dkpro.core_de.tudarmstadt.ukp.dkpro.core.arktools-gpl_1.8.0
-#className=de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter
 className=de.tudarmstadt.ukp.dkpro.core.arktools.ArktweetTokenizer
-inD=../testOutput/
-otD=../testOutput2/
+inD=../testOutput_de.tudarmstadt.ukp.dkpro.core.io.pdf.PdfReader/
+otD=../testOutput_$className/
 
 bash ./Linux_runDKPro.sh $coordinates $className $inD $otD
--- scripts/Linux_runDKProStanfordSegmenter.sh
+++ scripts/Linux_runDKProStanfordSegmenter.sh
@@ -1,6 +1,6 @@
 coordinates=de.tudarmstadt.ukp.dkpro.core_de.tudarmstadt.ukp.dkpro.core.stanfordnlp-gpl_1.8.0
 className=de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter
-inD=../testOutput/
-otD=../testOutput3/
+inD=../testOutput_de.tudarmstadt.ukp.dkpro.core.io.pdf.PdfReader/
+otD=../testOutput_$className/
 
 bash ./Linux_runDKPro.sh $coordinates $className $inD $otD
