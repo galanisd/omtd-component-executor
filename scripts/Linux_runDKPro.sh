86ffa110af61e7bed78845a7ccc973d6dd52a01a
ilsp <galanisd@gmail.com>
2017-08-05 02:25:12 +0300

    ** Added readability + StanfordPOSTagger.

--- scripts/Linux_runDKPro.sh
+++ scripts/Linux_runDKPro.sh
@@ -8,4 +8,4 @@ otDir=$4
 jarList=$(cat ../TDMClasspathLists/"classpath."$coordinates)
 echo ../TDMClasspathLists/"classpath."$coordinates
 #echo $jarList
-java -Dloader.path=$jarList -jar $(pwd)/../omtd-component-dkpro-1.8.0-uima-2.8.1/target/omtd-component-dkpro-1.8.0-uima-2.8.1-0.0.1-SNAPSHOT-exec.jar $className $inDir $otDir
+java -Xmx4096m -Dloader.path=$jarList -jar $(pwd)/../omtd-component-dkpro-1.8.0-uima-2.8.1/target/omtd-component-dkpro-1.8.0-uima-2.8.1-0.0.1-SNAPSHOT-exec.jar $className $inDir $otDir
