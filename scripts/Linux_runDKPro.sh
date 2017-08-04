fb7f11bc4189a98a9aa274ce51fa709909a6ad74
ilsp <galanisd@gmail.com>
2017-08-04 15:31:40 +0300

    ** Corrected script.

--- scripts/Linux_runDKPro.sh
+++ scripts/Linux_runDKPro.sh
@@ -8,4 +8,4 @@ otDir=$4
 jarList=$(cat ../TDMClasspathLists/"classpath."$coordinates)
 echo ../TDMClasspathLists/"classpath."$coordinates
 #echo $jarList
-java -Dloader.path=$jarList -jar $(pwd)/../omtd-component-dkpro-1.8.0-uima-2.8.1/target/omtd-component-dkpro-uima-1.9.0-0.0.1-SNAPSHOT-exec.jar $className $inDir $otDir
+java -Dloader.path=$jarList -jar $(pwd)/../omtd-component-dkpro-1.8.0-uima-2.8.1/target/omtd-component-dkpro-1.8.0-uima-2.8.1-0.0.1-SNAPSHOT-exec.jar $className $inDir $otDir
