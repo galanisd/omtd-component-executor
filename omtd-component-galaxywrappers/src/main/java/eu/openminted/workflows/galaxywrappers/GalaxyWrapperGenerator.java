9cc1e3158f0516352426b491c61db7bf8e74cfef
ilsp <galanisd@gmail.com>
2017-09-08 09:20:46 +0300

    * Updated Generator.

--- omtd-component-galaxywrappers/src/main/java/eu/openminted/workflows/galaxywrappers/GalaxyWrapperGenerator.java
+++ omtd-component-galaxywrappers/src/main/java/eu/openminted/workflows/galaxywrappers/GalaxyWrapperGenerator.java
@@ -30,51 +30,51 @@ public class GalaxyWrapperGenerator {
 
 	private OMTDSHAREParser omtdshareParser;
 	private GalaxyToolWrapperWriter galaxyToolWrapperWriter;
-	
+
 	private File outDirHandler;
-	
-	public GalaxyWrapperGenerator(String outDir){			
+
+	public GalaxyWrapperGenerator(String outDir) {
 		omtdshareParser = new OMTDSHAREParser();
 		galaxyToolWrapperWriter = new GalaxyToolWrapperWriter();
-		
-		// Create wrappers output dir if not exists. 
+
+		// Create wrappers output dir if not exists.
 		outDirHandler = new File(outDir);
-		if(!outDirHandler.exists()){
+		if (!outDirHandler.exists()) {
 			outDirHandler.mkdirs();
 		}
 	}
-	
-	public Tool generate(File omtdShareFile){
-		try{
-			// Parse omtd-share file.			
+
+	public Tool generate(File omtdShareFile) {
+		try {
+			// Parse omtd-share file.
 			Component componentMeta = omtdshareParser.parse(omtdShareFile);
 
 			// Get info
-			ComponentInfo componentInfo = componentMeta.getComponentInfo();			
+			ComponentInfo componentInfo = componentMeta.getComponentInfo();
 			List<Description> descriptions = componentInfo.getIdentificationInfo().getDescriptions();
-			
+
 			// ** Create the tool.
 			Tool tool = new Tool();
-			
+
 			// Get tool desc.
-			String desc  = "";
-			if(descriptions != null && descriptions.size() > 0){				
+			String desc = "";
+			if (descriptions != null && descriptions.size() > 0) {
 				desc = descriptions.get(0).getValue();
-			}else{
+			} else {
 				desc = componentInfo.getIdentificationInfo().getResourceNames().get(0).getValue();
 			}
 
 			// Get id.
 			String id = componentInfo.getIdentificationInfo().getResourceIdentifiers().get(0).getValue();
-			
+
 			String name = componentInfo.getIdentificationInfo().getResourceNames().get(0).getValue();
-					
+
 			name = name.substring(name.lastIndexOf(".") + 1);
 			// Get tool version.
 			String version = componentInfo.getVersionInfo().getVersion();
-			
+
 			// Configure wrapper description.
-			tool.setDescription(desc);			
+			tool.setDescription(desc);
 			tool.setId(id);
 			tool.setName(name);
 			tool.setVersion(version);
@@ -86,27 +86,25 @@ public class GalaxyWrapperGenerator {
 			container.setValue("snf-765691.vm.okeanos.grnet.gr/openminted/omtd-workflows-executor");
 			requirements.setContainer(container);
 			tool.setRequirements(requirements);
-			
-			tool.setCommand("to be completed");
-			
+
 			// Configure wrapper inputs/outputs.
 			Inputs inputs = new Inputs();
 			Outputs outputs = new Outputs();
-			
-			ProcessingResourceInfo  info = componentInfo.getInputContentResourceInfo();
-			
-			ArrayList<Param> inputParams = extractInputParams(info);
-			Param dataGalaxyParam = new Param();
-			dataGalaxyParam.setType("data");
-			dataGalaxyParam.setName(name + "_InputFiles");
-			dataGalaxyParam.setLabel(name + "_InputFiles");
-			dataGalaxyParam.setFormat("uknown");
-			dataGalaxyParam.setMultiple("true");
+
+			ProcessingResourceInfo info = componentInfo.getInputContentResourceInfo();
+
+			// Create input params
+			ArrayList<Param> inputParams = createInputParams(info);
+			// Create&add the data input param
+			Param dataGalaxyParam = createDataInputParam(name);
 			inputParams.add(dataGalaxyParam);
-			
+
 			inputs.setParams(inputParams);
 			tool.setInputs(inputs);
-			
+
+			// tool.setCommand("to be completed");
+			tool.setCommand(buildExecutionCommand(dataGalaxyParam.getName()));
+
 			DiscoverDatasets dd = new DiscoverDatasets();
 			dd.setDirectory("out");
 			dd.setPattern("__designation__");
@@ -118,54 +116,85 @@ public class GalaxyWrapperGenerator {
 			collection.setType("list");
 			collection.setLabel(name + "_output");
 			outputs.setCollection(collection);
-			//info = componentInfo.getOutputResourceInfo();
-			//outputs.setParams(extractInputParams(info));
-			 
-			
+			// info = componentInfo.getOutputResourceInfo();
+			// outputs.setParams(extractInputParams(info));
+
 			tool.setOutputs(outputs);
-			
+
 			// Serialize wrapper object to a file.
 			String galaxyWrapperPath = outDirHandler.getAbsolutePath() + "/" + omtdShareFile.getName() + ".xml";
 			galaxyToolWrapperWriter.write(tool, galaxyWrapperPath);
-			
+
 			return tool;
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			log.info(e.getMessage());
 			return null;
 		}
-	
+
 	}
-	
-	private ArrayList<Param> extractInputParams(ProcessingResourceInfo info){
-		
-		ArrayList<Param> params = new ArrayList<Param>();		
+
+	private Param createDataInputParam(String name) {
+		Param dataGalaxyParam = new Param();
+
+		// dataGalaxyParam.setType("data");
+		dataGalaxyParam.setType("data_collection");
+		dataGalaxyParam.setCollection_type("list");
+
+		dataGalaxyParam.setName(name + "_InputFiles");
+		dataGalaxyParam.setLabel(name + "_InputFiles");
+		dataGalaxyParam.setFormat("uknown");
+
+		// dataGalaxyParam.setMultiple("true");
+
+		return dataGalaxyParam;
+	}
+
+	private String buildExecutionCommand(String inputDirVar) {
+		StringBuilder command = new StringBuilder();
+		command.append("\n");
+		command.append("mkdir tmp;\n");
+		command.append("#for $file in $" + inputDirVar + "\n");
+		command.append("\t");
+		command.append("cp $file tmp/$file.element_identifier;\n");
+		command.append("#end for\n");
+		command.append("Linux_runDKPro.sh tmp $output.job_working_directory/working/out/");
+
+		return command.toString();
+	}
+
+	/**
+	 * Creates the input parameter for a Galaxy tool.
+	 * @param info
+	 * @return the list of parameters.
+	 */
+	private ArrayList<Param> createInputParams(ProcessingResourceInfo info) {
+
+		ArrayList<Param> params = new ArrayList<Param>();
 		List<ParameterInfo> parametersInfos = info.getParameterInfos();
-		
-		for(ParameterInfo paramInfo : parametersInfos){
+
+		for (ParameterInfo paramInfo : parametersInfos) {
 			Param galaxyParam = new Param();
-			
+
 			galaxyParam.setName(paramInfo.getParameterName());
 			galaxyParam.setLabel(paramInfo.getParameterLabel());
 			galaxyParam.setDescription(paramInfo.getParameterDescription());
 			galaxyParam.setOptional(String.valueOf(paramInfo.isOptional()));
-			
+
 			// Set default value.
-			if(paramInfo.getDefaultValue() != null && paramInfo.getDefaultValue().size() > 0){
+			if (paramInfo.getDefaultValue() != null && paramInfo.getDefaultValue().size() > 0) {
 				galaxyParam.setValue(paramInfo.getDefaultValue().get(0));
 			}
 
 			// https://docs.galaxyproject.org/en/master/dev/schema.html
 			// Use only "text" for now.
 			galaxyParam.setType(GalaxyCons.text);
-			
+
 			// Add it to the list.
 			params.add(galaxyParam);
-		}		
-		
+		}
+
 		return params;
 	}
-	
-	
 
 }
