a78f8ab2f0fb12527949172d8ab7e07bb8b0456b
ilsp <galanisd@gmail.com>
2017-09-09 19:28:25 +0300

    * Updated.

--- omtd-workflows-createDockerImg.sh
+++ omtd-workflows-createDockerImg.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
+clear
 DockerRegistyHOST=$1
 
 DockerImg="omtd-workflows-executor"
 Dockerfile="./omtd-workflows-executor.dockerfile"
 DockerImgTag="${DockerRegistyHOST}/openminted/${DockerImg}"
 
+echo "-----"
 echo "Dockerfile:"$Dockerfile 
 echo "DockerImg:"$DockerImg
 echo "DockerImgTag:"$DockerImgTag
+echo "-----"
 
 # Build project
 #mvn clean install
 
-# TBA: Generate wrappers&TDMCoordinatesList from omtd descriptors 
+# TBA
+# Generate Galaxy Wrappers & TDMCoordinatesList  file from omtd-share descriptors
+echo "Generate galaxy wrappers and TDMCoordinatesList" 
 
-# TBA: Copy wrappers to target machine/dir 
-
-# Build image
+# Build image.
 docker build -f $Dockerfile -t $DockerImg .
 
-# Tag the image 
+# Tag the image.
+echo "Tag image" 
 docker tag -f $DockerImg $DockerImgTag
 
-# Push
-docker $DockerImgTag
+# Push it to Registry.
+echo "Push image"
+docker push $DockerImgTag
+
+# TBA: Now that image is pushed copy wrappers to target machine/dir
+# so that everything appears in Galaxy UI. 
+echo "Copying wrappers"
+
+ 
+
