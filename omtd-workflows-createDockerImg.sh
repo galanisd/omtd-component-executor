e02abeb7391f7275a6af6016e5c0e2dc4f84294d
ilsp <galanisd@gmail.com>
2017-09-09 16:05:28 +0300

    * Updated createImg script.

--- omtd-workflows-createDockerImg.sh
+++ omtd-workflows-createDockerImg.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-Dockerfile="./omtd-workflows-executor.dockerfile"
+DockerRegistyHOST=$1
+
 DockerImg="omtd-workflows-executor"
+Dockerfile="./omtd-workflows-executor.dockerfile"
+DockerImgTag="${DockerRegistyHOST}/openminted/${DockerImg}"
+
+echo "Dockerfile:"$Dockerfile 
+echo "DockerImg:"$DockerImg
+echo "DockerImgTag:"$DockerImgTag
 
 # Build project
 #mvn clean install
 
+# TBA: Generate wrappers&TDMCoordinatesList from omtd descriptors 
+
+# TBA: Copy wrappers to target machine/dir 
+
 # Build image
 docker build -f $Dockerfile -t $DockerImg .
 
+# Tag the image 
+docker tag -f $DockerImg $DockerImgTag
+
+# Push
+docker $DockerImgTag
