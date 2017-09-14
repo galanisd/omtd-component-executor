bc0cf25488444c62281992adc2171360c9192d45
ilsp <galanisd@gmail.com>
2017-09-14 13:24:10 +0300

    * Reorganized

--- scripts/examplesGATE/Linux_runGATEANNIESplitter.sh
+++ scripts/examplesGATE/Linux_runGATEANNIESplitter.sh
@@ -4,4 +4,4 @@ maven="uk.ac.gate.plugins:annie:8.5-SNAPSHOT"
 classname=gate.creole.splitter.SentenceSplitter
 inD=../testInput/
 otD=../testOutput-annie/
-./Linux_runGATE.sh $maven $classname $inD $otD
+../Linux_runGATE.sh $maven $classname $inD $otD
--- scripts/examplesGATE/Linux_runGATEANNIETokeniser.sh
+++ scripts/examplesGATE/Linux_runGATEANNIETokeniser.sh
@@ -4,4 +4,4 @@ maven="uk.ac.gate.plugins:annie:8.5-SNAPSHOT"
 classname=gate.creole.tokeniser.DefaultTokeniser
 inD=../testInput/
 otD=../testOutput-annie/
-./Linux_runGATE.sh $maven $classname $inD $otD
+../Linux_runGATE.sh $maven $classname $inD $otD
