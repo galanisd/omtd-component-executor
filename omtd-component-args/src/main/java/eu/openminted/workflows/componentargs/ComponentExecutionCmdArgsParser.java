6056c8640a0a0459f479ad9ff3463c8e991a4b37
ilsp <galanisd@gmail.com>
2017-09-27 01:40:19 +0300

    * Gets a list of -Pkey=value params.

--- omtd-component-args/src/main/java/eu/openminted/workflows/componentargs/ComponentExecutionCmdArgsParser.java
+++ omtd-component-args/src/main/java/eu/openminted/workflows/componentargs/ComponentExecutionCmdArgsParser.java
@@ -1,5 +1,8 @@
 package eu.openminted.workflows.componentargs;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -49,9 +52,20 @@ public class ComponentExecutionCmdArgsParser {
                 .desc(" the class name")
                 .build();
 		
+		Option param = Option.builder("P")
+				.argName("key=value")
+				.required(false)
+				.hasArgs()
+				.numberOfArgs(2)
+				.valueSeparator('=')
+				.desc("sample")
+				.build();
+
+						
 		options.addOption(input);
 		options.addOption(output);
 		options.addOption(className);
+		options.addOption(param);
 				
 	}
 	
@@ -60,11 +74,20 @@ public class ComponentExecutionCmdArgsParser {
 	    
 		try {
 	        givenCMD = parser.parse(options, args, false);
-	        System.err.println(givenCMD.getArgList().size());
 	        
-	        System.err.println(givenCMD.hasOption("input") + " " + givenCMD.getOptionValue("input"));
-	        System.err.println(givenCMD.hasOption("output") + " " + givenCMD.getOptionValue("output"));
-	        System.err.println(givenCMD.hasOption("className") + " " + givenCMD.getOptionValue("className"));
+	        System.err.println("input" + " " + givenCMD.getOptionValue("input"));
+	        System.err.println("output" + " " + givenCMD.getOptionValue("output"));
+	        System.err.println("className" + " " + givenCMD.getOptionValue("className"));
+	        
+	        Set<String> set = givenCMD.getOptionProperties("P").stringPropertyNames();
+	        
+	        Iterator<String> it = set.iterator();
+	        while(it.hasNext()){
+	        	String propertyName = it.next();
+	        	String propertyValue = givenCMD.getOptionProperties("P").getProperty(propertyName);
+	        
+	        	System.err.println(propertyName + " " + propertyValue );
+	        }
 	        
 	        return givenCMD;
 	    } catch (ParseException e) {
--- omtd-component-args/src/main/java/eu/openminted/workflows/componentargs/ComponentExecutionCmdArgsParserExample.java
+++ omtd-component-args/src/main/java/eu/openminted/workflows/componentargs/ComponentExecutionCmdArgsParserExample.java
@@ -3,7 +3,7 @@ package eu.openminted.workflows.componentargs;
 public class ComponentExecutionCmdArgsParserExample {
 	
 	public static void main(String args[]){
-		String testArgs [] = {"-input", "inF", "-output", "outF", "-className" , "eu.openminted.MyLemmatizer"};		
+		String testArgs [] = {"-input", "inF", "-output", "outF", "-className" , "eu.openminted.MyLemmatizer", "-Plang=en", "-Pmodel=stanford"};		
 		//String testArgs [] = {"-input", "inF", "-output", "outF", "-clssName" , "eu.openminted.MyLemmatizer"};
 		
 		ComponentExecutionCmdArgsParser parser = new ComponentExecutionCmdArgsParser();
