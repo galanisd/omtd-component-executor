f9b2a074fd1b19a0fd4c865774cdf6ae11324eb0
galanisd <galanisd@gmail.com>
2017-09-27 17:52:46 +0300

    * Now a reader/writer can be parameterized.

--- omtd-component-uima-2.8.1/src/main/java/eu/openminted/workflows/uima/executor/UIMAFitRunner.java
+++ omtd-component-uima-2.8.1/src/main/java/eu/openminted/workflows/uima/executor/UIMAFitRunner.java
@@ -3,6 +3,9 @@ package eu.openminted.workflows.uima.executor;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 import org.apache.uima.analysis_component.AnalysisComponent;
 import org.apache.uima.analysis_engine.AnalysisEngine;
@@ -15,6 +18,10 @@ import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import eu.openminted.workflows.componentargs.ComponentArgs;
 
+/**
+ * @author galanisd
+ *
+ */
 public class UIMAFitRunner {
 	
 	public void uimaFitRun(ComponentArgs componentArgs) throws Exception{
@@ -42,19 +49,31 @@ public class UIMAFitRunner {
 		
 		// If so, only read and write. (Read -> Write)
 		if(classThatExtendsCollectionReader != null){	
-			//reader = CollectionReaderFactory.createReader(classThatExtendsCollectionReader, ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, inputDir);			
-			reader = CollectionReaderFactory.createReader(classThatExtendsCollectionReader, ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, inputDir, 
-					ResourceCollectionReaderBase.PARAM_PATTERNS, "[+]**/*.pdf", // hardwired: TO-BE-CHANGED
-					ResourceCollectionReaderBase.PARAM_LANGUAGE, "en"); // hardwired: TO-BE-CHANGED
+			
+			// this works but some things are hardwired.
+			//reader = CollectionReaderFactory.createReader(classThatExtendsCollectionReader, 
+			//			ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, inputDir, 
+			//			ResourceCollectionReaderBase.PARAM_PATTERNS, "[+]**/*.pdf", // hardwired: TO-BE-CHANGED
+			//			ResourceCollectionReaderBase.PARAM_LANGUAGE, "en"); // hardwired: TO-BE-CHANGED
 
+			// !!
+			reader = CollectionReaderFactory.createReader(classThatExtendsCollectionReader, getParamsForUIMA(componentArgs, true));					
+			
 			engines = new AnalysisEngine[1];
 			engines[0] = writerEngine;
 	
 		}else{// Otherwise: Read -> Process -> Write
-			reader = CollectionReaderFactory.createReader(XmiReader.class, ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, inputDir, 
-					ResourceCollectionReaderBase.PARAM_PATTERNS, "[+]**/*.xmi");
 			
-			AnalysisEngine componentEngine = createEngine(getComponent(klass));
+			
+			reader = CollectionReaderFactory.createReader(XmiReader.class, 
+						ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, inputDir, 
+						ResourceCollectionReaderBase.PARAM_PATTERNS, "[+]**/*.xmi");
+			
+			// 	this works but some things are hardwired.
+			// AnalysisEngine componentEngine = createEngine(getComponent(klass));
+			
+			// !!
+			AnalysisEngine componentEngine = createEngine(getComponent(klass), getParamsForUIMA(componentArgs, false));
 					
 			engines = new AnalysisEngine[2];			
 			engines[0] = componentEngine;
@@ -93,4 +112,41 @@ public class UIMAFitRunner {
 		return outputFolderFile;
 	}
 
+	public static Object[] getParamsForUIMA(ComponentArgs componentArgs, boolean includeInput){
+				
+		Object[] nonInputParameters = getParamsForUIMA(componentArgs);
+		
+		if(includeInput){							
+			// Input params	
+			// We need a 2 * numOfUIMAParams table for them. 
+			Object[] inputParam = new Object[2];			
+			inputParam[0] = ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION;
+			inputParam[1] = componentArgs.getInput();
+			
+			Object[] allParameters = Stream.concat(Arrays.stream(inputParam), Arrays.stream(nonInputParameters)).toArray(String[]::new);
+			return allParameters;
+		}else{
+			return nonInputParameters;
+		}
+				
+	}
+	
+	public static Object[] getParamsForUIMA(ComponentArgs componentArgs){
+		int numOfUIMAParams = componentArgs.getParameters().size();
+		
+		// We need a 2 * numOfUIMAParams table for them. 
+		Object[] params = new Object[ 2 * numOfUIMAParams];		
+				
+		Iterator<String> it = componentArgs.getParameters().keySet().iterator();
+		for(int i = 0; i < params.length; i = i + 2){
+			String paramName = it.next();
+			String paramValue = componentArgs.getParameters().get(paramName);
+			
+			params[i] = paramName;
+			params[i+1] = paramValue;
+		}
+		
+		return params;
+	}
+	
 }
