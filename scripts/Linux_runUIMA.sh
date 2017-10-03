9d502c9c2e99301b36f28b3b8610278a0261ef19
ilsp <galanisd@gmail.com>
2017-10-03 18:35:42 +0300

    * Added comments.

--- scripts/Linux_runUIMA.sh
+++ scripts/Linux_runUIMA.sh
@@ -63,4 +63,5 @@ jarList=$(cat $componentExecutorInstallationDir/TDMClasspathLists/"classpath."$c
 #done
 
 #echo $jarList
+# Call executor which is a java Spring-Boot based executable. 
 java -Xmx4096m -Dloader.path=$jarList -jar $componentExecutorInstallationDir/omtd-component-uima-2.8.1/target/omtd-component-uima-2.8.1-0.0.1-SNAPSHOT-exec.jar -className $className -input $inDir -output $otDir $uimaParams 
--- scripts/examplesUIMADKPro/Linux_runDKProStanfordPOSTagger.sh
+++ scripts/examplesUIMADKPro/Linux_runDKProStanfordPOSTagger.sh
@@ -1,6 +1,7 @@
-coordinates=de.tudarmstadt.ukp.dkpro.core_de.tudarmstadt.ukp.dkpro.core.stanfordnlp-gpl_1.8.0
+# !!!! Works for small inputs. 
+coordinates=de.tudarmstadt.ukp.dkpro.core_de.tudarmstadt.ukp.dkpro.core.stanfordnlp-gpl_1.9.0-SNAPSHOT
 className=de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger
 inD=../testOutput_de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter/
 otD=../testOutput_$className/
 
-bash ../Linux_runUIMA.sh $coordinates $className $inD $otD
+bash ../Linux_runUIMA.sh $coordinates $className -input $inD -output $otD
