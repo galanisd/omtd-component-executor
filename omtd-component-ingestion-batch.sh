cb72ca59f9cbb74d5b8d13306a25a1fc0c13d4a0
stefanos <stevengatsios@gmail.com>
2017-12-06 14:43:57 +0200

    update

--- omtd-component-ingestion-batch.sh
+++ omtd-component-ingestion-batch.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
-./omtd-component-ingestionGen.sh ingestion.docker.example.conf
-./omtd-component-ingestionGen.sh ingestion.GATE.example2.conf
-./omtd-component-ingestionGen.sh ingestion.UIMA.example2.conf
+monitoredDir="/var/lib/docker/volumes/stack_registrydata/_data/"
+dir="ComponentsForGalaxy"
+
+if [ ! -d "$dir" ]; then
+	mkdir $dir
+	cd $dir
+	mkdir "UIMA"
+	mkdir "GATE"
+	mkdir "Docker"
+fi
+
+for x in `find $monitoredDir -type f`
+do
+	echo $x
+	if grep -q 'framework>UIMA' "$x"; then
+   		echo "UIMA"
+		cp $x $dir/UIMA/
+ 	elif grep -q 'framework>GATE' "$x";
+	then
+		echo "GATE" 
+		 cp $x $dir/GATE/
+	else
+		echo "Docker"
+		 cp $x $dir/Docker/
+	fi
+done
+
+./omtd-component-ingestionGen.sh ingestion.docker.conf
+./omtd-component-ingestionGen.sh ingestion.GATE.conf
+./omtd-component-ingestionGen.sh ingestion.UIMA.conf
