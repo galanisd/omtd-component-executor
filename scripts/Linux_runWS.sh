a4cfbfeea97392e2355c4a44f7761095e5b15b91
ilsp <galanisd@gmail.com>
2018-03-02 19:13:29 +0200

    * WS executor ready

--- scripts/Linux_runWS.sh
+++ scripts/Linux_runWS.sh
@@ -12,11 +12,10 @@ else
 fi
 
 # We need the following to call the WS executor.
-#coordinates=$1
-#className=$2
 inDir=""
 otDir=""
 wsParams=""
+typesystems=""
 
 # Parse arguments and fill.
 while [[ $# -gt 0 ]]
@@ -35,7 +34,12 @@ case $key in
     shift # past value
     ;;
     -P*)
+    echo "###:"$1
     wsParams=$wsParams" "$1
+    #if [[ $1 == "-Ptypesystems=" ]]  
+  	#then  
+  		typesystems=$1
+	#fi
     shift # past argument
     ;;
     *)    # unknown option
@@ -44,27 +48,48 @@ case $key in
 esac
 done
 
-#dir=$(pwd)
-#if [ ! -d "$componentExecutorInstallationDir/TDMlocalRepo" ]; then
-#	ocoord=${coordinates//_/:}
-#	echo "ocoord:"$ocoord
-#	echo $ocoord > $componentExecutorInstallationDir/TDMCoordinatesList.txt
-#	cd $componentExecutorInstallationDir
-#	bash FetchDependenciesUIMA.sh
-#	cd $dir
-#else
-#	echo "$locRepo exists...no need to fetch deps."
-#fi
-
-#echo "coordinates:"$coordinates
+echo "wsParams:"$wsParams
 echo "inDir:"$inDir
 echo "otDir:"$otDir
-echo "wsParams:"$wsParams
+echo "typesystems:"$typesystems
+typesystems=${typesystems:14}
+echo "typesystems:"$typesystems
 
-# Retrieve dependencies jar list.
-#jarList=$(cat $componentExecutorInstallationDir/TDMClasspathLists/"classpath."$coordinates)
-#echo $jarList
+dir=$(pwd)
+# Clear TDMCoordinatesList.txt
+rm $componentExecutorInstallationDir/TDMCoordinatesList.txt
+# Clear TDMClasspathLists.
+rm $componentExecutorInstallationDir/TDMClasspathLists/*
+# For each ts 
+for ts in $(echo $typesystems | tr ";" "\n")
+do
+  # process
+  echo "ts:"$ts
+  ocoord=${ts//_/:}
+  echo $ocoord >> $componentExecutorInstallationDir/TDMCoordinatesList.txt
+done
+
+# Fetch dependencies.
+cd $componentExecutorInstallationDir
+bash FetchDependenciesUIMA.sh	
+cd $dir
+
+# Build jar list
+jarList=""
+for f in $componentExecutorInstallationDir/TDMClasspathLists/*
+do
+	echo "Processing $f"
+	
+	if [ -z "$jarList" ]
+	then
+		jarList=$(cat $f)
+	else
+		jarList=$(cat $f)","$jarList
+	fi
+	
+done
+
+echo $jarList
 
 # Call executor which is a java Spring-Boot based executable. 
-#java -Xmx4096m -Dloader.path=$jarList -jar $componentExecutorInstallationDir/omtd-component-webservice/target/omtd-component-webservice-0.0.1-SNAPSHOT-exec.jar -input $inDir -output $otDir $wsParams 
-java -Xmx4096m -jar $componentExecutorInstallationDir/omtd-component-webservice/target/omtd-component-webservice-0.0.1-SNAPSHOT-exec.jar -input $inDir -output $otDir $wsParams 
+java -Xmx4096m -Dloader.path=$jarList -jar $componentExecutorInstallationDir/omtd-component-webservice/target/omtd-component-webservice-0.0.1-SNAPSHOT-exec.jar -input $inDir -output $otDir $wsParams 
--- scripts/examplesWS/Linux_runWSChebi.sh
+++ scripts/examplesWS/Linux_runWSChebi.sh
@@ -1,5 +1,6 @@
-url="http://nactem.ac.uk/api/openminted/neuroscience"
-inD=../testOutput_de.tudarmstadt.ukp.dkpro.core.io.pdf.PdfReader/
+url="http://nactem.ac.uk/api/openminted/chebi"
+ts="uk.ac.nactem.uima_ChebiCurationTypeSystem_0.1"
+inD=../testOutput_de.tudarmstadt.ukp.dkpro.core.io.pdf.PdfReader2/
 otD=../testOutput_Chebi/
 
-bash ../Linux_runWS.sh -input $inD -output $otD -Pwsurl=http://nactem.ac.uk/api/openminted/chebi
+bash ../Linux_runWS.sh -input $inD -output $otD -Pwsurl=$url -Ptypesystems=$ts
