6456f9661a51ca915c8d947fb08a062d066c4d4d
ilsp <galanisd@gmail.com>
2018-03-20 12:30:09 +0200

    * No issue. Refactored.

--- scriptsUtil/OMTDRegistry.sh
+++ scriptsUtil/OMTDRegistry.sh
@@ -6,19 +6,19 @@ echo "Reading config...." >&2
 . $dir$1
 echo -e "Started.." $dir$1
 
-service1=$baseURL/$resourceType/$resourceID
-service2=$baseURL/$resourceType/
-echo "service1:"$service1
+serviceDown=$baseURL/$resourceType/$resourceID
+serviceDele=$baseURL/$resourceType/
+echo "serviceDown:"$serviceDown
 
 # Download resource xml.
-curl -H "Accept: application/xml" -X "GET" $service1 -o resourceMetadata.xml
+curl -H "Accept: application/xml" -X "GET" $serviceDown -o resourceMetadata.xml
 # Download resource json.
-curl -H "Accept: application/json" -X "GET" $service1 -o resourceMetadata.json
+curl -H "Accept: application/json" -X "GET" $serviceDown -o resourceMetadata.json
 #echo "\n" >> resourceMetadata.json
 
 # Delete it
 if [ $delete == "yes" ]; then
-	echo "service2:"$service2
-	curl -H "Content-Type: application/json" -X "DELETE" $service2 -d @resourceMetadata.json 
+	echo "serviceDele:"$serviceDele
+	curl -H "Content-Type: application/json" -X "DELETE" $serviceDele -d @resourceMetadata.json 
 	echo "deleting:"$delete
 fi
