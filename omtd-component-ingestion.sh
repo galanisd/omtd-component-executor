9ade11c7d10f1b8a0106cede5e34a363ec0a024c
ilsp <galanisd@gmail.com>
2018-04-25 14:36:07 +0300

    * Latest version.

--- omtd-component-ingestion.sh
+++ omtd-component-ingestion.sh
@@ -62,6 +62,7 @@ echo "-----"
 # * Galaxy Wrappers 
 # * TDMCoordinatesList file (only for Maven-based components)
 # from omtd-share descriptors
+# ~~~~~~~~~~~~~~~~~~~~~
 #suffix=".wrapper."$ComponentID"."$ComponentVersion".xml"
 suffix=".xml"
 echo "Generate galaxy wrappers and TDMCoordinatesList" 
@@ -81,26 +82,42 @@ if [ $Dockerfile != "none" ]; then
 	sudo docker build -f $Dockerfile -t $DockerImg .
 fi
 
+# If specified Tag and Push image.
+# ~~~~~~~~~~~~~~~~~~~~~
 echo "Push:"$Push
 if [ $Push == "yes" ]; then
 	# Tag the image for OMTD docker registry.
 	echo "-- -- Tag image" 
-	sudo docker tag -f $DockerImg $DockerImgTag
+	#sudo docker tag -f $DockerImg $DockerImgTag
+	sudo docker tag $DockerImg $DockerImgTag
 
 	# Push it to OMTD docker Registry.
 	echo "-- -- Push image:"$DockerImgTag
 	sudo docker push $DockerImgTag
 fi
 
-# Now that image is pushed and is available to docker registry 
-# copy wrappers to target machine/dir so that everything appears in Galaxy UI. 
-echo "-- -- Copying wrappers"
-wrappersDest=root@$GalaxyExecutorLoc:/srv/galaxy/tools/$GalaxyID
-echo $wrappersDir
-echo $wrappersDest
-
-# Copying to Executor:
-scp -r $wrappersDir/* $wrappersDest
-# Copying to Editor: The tools dirs of Editor and Executor are synced via NFS so copying to Editor is deactivated.
-#scp -r $wrappersDir/* root@snf-1480.ok-kno.grnetcloud.net:/srv/galaxy/tools/$GalaxyID 
+# Ask
+echo -e "\n\n"
+read -p "Copy wrappers to Executor (y/n)?" choice
+case "$choice" in 
+  y|Y ) 
+  echo "yes"
+  # Now that image is pushed and is available to docker registry 
+  # copy wrappers to target machine/dir so that everything appears in Galaxy UI. 
+  # ~~~~~~~~~~~~~~~~~~~~~
+  echo "-- -- Copying wrappers"
+  wrappersDest=root@$GalaxyExecutorLoc:/srv/galaxy/tools/$GalaxyID
+  echo $wrappersDir
+  echo $wrappersDest
+  # Copying to Executor:
+  scp -r $wrappersDir/* $wrappersDest
+  # Copying to Editor: The tools dirs of Editor and Executor are synced via NFS so copying to Editor is deactivated.
+  #scp -r $wrappersDir/* root@snf-1480.ok-kno.grnetcloud.net:/srv/galaxy/tools/$GalaxyID 
+  ;;
+  n|N ) 
+  echo "wrappers ... not copied";;
+  * ) 
+  echo "invalid"
+  ;;
+esac
 
